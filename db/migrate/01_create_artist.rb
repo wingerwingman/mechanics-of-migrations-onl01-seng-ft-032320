8f3123e69bb9cadd86d91e5f8c3c33e268300b20
wingerwingman <justinwinger4@gmail.com>
2020-04-27 09:19:54 -0600

    Done.

---
+++ db/migrate/01_create_artist.rb
@@ -0,0 +1,19 @@
+class CreateArtist < ActiveRecord::Migration[5.2]
+    def up 
+    end
+
+    def change
+    end
+
+    def down 
+    end
+
+    def change
+        create_table :artists do |t|
+          t.string :name
+          t.string :genre
+          t.integer :age
+          t.string :hometown
+        end
+      end
+end
